d7a173e5fd6fd0692e15f388643f433da3309f5e
Raphael Vacari <raphaelvacari7@gmail.com>
2023-05-22 14:16:37 -0300

    voltando

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação Está se Conectando ao Banco de Dados")
-// Console de teste
-console.log("O Paulo fez esse commit sem voce ver na aula")
-console.log("Esse é um teste meu")
