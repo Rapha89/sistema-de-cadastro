eaaf54ba71b35010ff916dbf69e97c59a25f2554
Raphael Vacari <raphaelvacari7@gmail.com>
2023-05-22 14:02:45 -0300

    commit teste app js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação Está se Conectando ao Banco de Dados")
+// Console de teste
+console.log("O Paulo fez esse commit sem voce ver na aula")
